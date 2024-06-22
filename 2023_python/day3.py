2307671da493fa3be06de68a07d0801d6911ce4a
Manicharan <manicharan150@gmail.com>
2024-06-22 11:21:25 +0530

    Some code redution

--- 2023_python/day3.py
+++ 2023_python/day3.py
@@ -1,57 +1,32 @@
-import re
-visited = list()
-
-
 def CharacterLocater(pattern):
     # This function takes a pattern as input and returns the location of Special characters and numbers in the pattern
     # The pattern is a string of characters
-    characters = ['#', '$', '%', '&', '*', '+', '-', '/', '=', '@']
-    numbers = '^[0-9]+$'
-    dirs = [
-            [-1, 1], [0, 1], [1, 1],
-            [-1, 0], [1, 0],
-            [-1, -1], [0, -1], [1, -1]
-    ]
-    numbers_encountered = []
-    special_characters_encountered = []
+    visited = set()
     i = 0
-    j = 0
-    total_sum = 0
     for i in range(len(pattern)):
+        j = 0
         for j in range(len(pattern[i])):
-            for character in characters:
-                if character in pattern[i][j]:
-                    special_characters_encountered.append((i, j))
-            if re.match(numbers, pattern[i][j]):
-                numbers_encountered.append((i, j))
+            if pattern[i][j].isdigit() or pattern[i][j] == ".":
+                continue
+            for current_row in [i - 1, i, i + 1]:
+                for current_coloumn in [j - 1, j, j + 1]:
+                    if current_row < 0 or current_row >= len(pattern) or current_coloumn < 0 or current_coloumn >= len(pattern[current_row]) or not pattern[current_row][current_coloumn].isdigit():
+                        continue
+                    while current_coloumn > 0 and pattern[current_row][current_coloumn - 1].isdigit():
+                        current_coloumn = current_coloumn - 1
+                    visited.add((current_row, current_coloumn))
 
-    for a, b in special_characters_encountered:
-        for x, y in dirs:
-            if 0 <= a + x < len(pattern) and 0 <= b + y < len(pattern[0]):
-                if (a + x, b + y) in numbers_encountered and (a + x, b + y) not in visited:
-                    total_sum += NumberFinder(pattern, a + x, b + y)
+    summation = []
+    for r, c in visited:
+        number = ""
+        while c < len(pattern[r]) and pattern[r][c].isdigit():
+            number += pattern[r][c]
+            c = c + 1
+        print(number)
+        summation.append(int(number))
 
-    print(total_sum)
-
-
-def NumberFinder(line, x, y):
-    i = y
-    sum = ''
-    while line[x][i] != '.':
-        if line[x][i].isdigit():
-            sum = line[x][i] + sum
-            visited.append((x, i))
-        i = i - 1
-    j = y + 1
-    while line[x][j] != '.' and j < len(line[x]):
-        if line[x][j].isdigit():
-            sum += line[x][j]
-            visited.append((x, j))
-        j = j + 1
-        if j == len(line[x]):
-            break
-    sum = int(sum)
-    return sum
+    print(summation)
+    print(sum(summation))
 
 
 file = open('input3.txt', 'r')
