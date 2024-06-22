4680817cb3c302bb989f81ee4aa9a0dbc148f5b4
Manicharan <manicharan150@gmail.com>
2024-06-22 09:12:02 +0530

    Added a function to get Neighbouring Numbers

--- 2023_python/day3.py
+++ 2023_python/day3.py
@@ -1,15 +1,22 @@
 import re
+visited = list()
 
 
 def CharacterLocater(pattern):
     # This function takes a pattern as input and returns the location of Special characters and numbers in the pattern
     # The pattern is a string of characters
-    characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', '{', '}', '[', ']', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '?', '/', '`', '~']
+    characters = ['#', '$', '%', '&', '*', '+', '-', '/', '=', '@']
     numbers = '^[0-9]+$'
+    dirs = [
+            [-1, 1], [0, 1], [1, 1],
+            [-1, 0], [1, 0],
+            [-1, -1], [0, -1], [1, -1]
+    ]
     numbers_encountered = []
     special_characters_encountered = []
     i = 0
     j = 0
+    total_sum = 0
     for i in range(len(pattern)):
         for j in range(len(pattern[i])):
             for character in characters:
@@ -18,8 +25,33 @@ def CharacterLocater(pattern):
             if re.match(numbers, pattern[i][j]):
                 numbers_encountered.append((i, j))
 
-    print(f"Special Characters Encountered at: {special_characters_encountered}")
-    print(numbers_encountered)
+    for a, b in special_characters_encountered:
+        for x, y in dirs:
+            if 0 <= a + x < len(pattern) and 0 <= b + y < len(pattern[0]):
+                if (a + x, b + y) in numbers_encountered and (a + x, b + y) not in visited:
+                    total_sum += NumberFinder(pattern, a + x, b + y)
+
+    print(total_sum)
+
+
+def NumberFinder(line, x, y):
+    i = y
+    sum = ''
+    while line[x][i] != '.':
+        if line[x][i].isdigit():
+            sum = line[x][i] + sum
+            visited.append((x, i))
+        i = i - 1
+    j = y + 1
+    while line[x][j] != '.' and j < len(line[x]):
+        if line[x][j].isdigit():
+            sum += line[x][j]
+            visited.append((x, j))
+        j = j + 1
+        if j == len(line[x]):
+            break
+    sum = int(sum)
+    return sum
 
 
 file = open('input3.txt', 'r')
